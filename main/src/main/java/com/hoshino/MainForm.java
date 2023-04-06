f5f74697d5ea703ea562bbcec04919649184d186
gitbadass <2606451878@qq.com>
2023-04-06 13:26:32 +0800

    test flatlaf

--- main/src/main/java/com/hoshino/MainForm.java
+++ main/src/main/java/com/hoshino/MainForm.java
@@ -3,25 +3,24 @@ package com.hoshino;
 
 import com.formdev.flatlaf.FlatDarculaLaf;
 import com.formdev.flatlaf.FlatLightLaf;
-import com.formdev.flatlaf.ui.FlatInternalFrameUI;
-import com.formdev.flatlaf.ui.FlatRootPaneUI;
-import com.formdev.flatlaf.ui.FlatTitlePane;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class MainForm {
     private static void createAndShowGUI() {
-        //确保一个漂亮的外观风格
-        //FlatLightLaf.setup();
-
         //创建及设置窗口
-        JFrame frame = new JFrame("Flatlaf");
+        JFrame frame = new JFrame("BADASS バイダス");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setIconImage(new ImageIcon("main/src/main/resources/icon.png").getImage());
 
         //添加切换主题的按钮
         JButton blackTheme = new JButton("black");//暗色
         JButton lightTheme = new JButton("light");//亮色
+
+        //添加文字板
+        JLabel label = new JLabel("Test 测试");
+        label.putClientProperty( "FlatLaf.styleClass", "h1" );//设置文字样式
 
         //暗色主题切换按钮监听事件
         blackTheme.addActionListener(e->{
@@ -35,24 +34,53 @@ public class MainForm {
             SwingUtilities.updateComponentTreeUI(frame);
         });
 
-        //添加组件到frame
-        frame.getContentPane().add(blackTheme);
-        frame.getContentPane().add(lightTheme);
-
         //设置组件布局
         frame.setLayout(new FlowLayout(FlowLayout.LEADING, 20, 20));
+
+        //更深层次的主题更改；操作标题栏
+        System.setProperty( "flatlaf.menuBarEmbedded", "true" );
+        System.setProperty( "flatlaf.useWindowDecorations", "true" );
+        JMenuBar menuBar = new JMenuBar();//创建菜单栏
+        JMenu fileMenu = new JMenu("文件");//文件菜单
+        JMenu edit = new JMenu("编辑");//编辑菜单
+        JMenu settings = new JMenu("设置");//编辑菜单
+        //创建属于 文件菜单 的菜单项
+        JMenuItem open = new JMenuItem("打开...");//打开...
+        open.addActionListener(e->{
+            // 打开... 菜单项的事件监听
+            System.out.println("打开文件");
+        });
+
+        JMenuItem save = new JMenuItem("保存");//保存
+        save.addActionListener(e->{
+            // 打开... 菜单项的事件监听
+            System.err.println("文件保存失败");
+            System.err.println("正在尝试退出...");
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException ex) {
+                throw new RuntimeException(ex);
+            }
+            System.exit(0);
+        });
+        //添加到 文件菜单 中
+        fileMenu.add(open);
+        fileMenu.add(save);
+
+        //添加内容到菜单栏
+        menuBar.add(fileMenu);
+        menuBar.add(edit);
+        menuBar.add(settings);
+
+        //添加组件到frame
+        frame.setJMenuBar(menuBar);
+        frame.add(label);
+        frame.getContentPane().add(blackTheme);
+        frame.getContentPane().add(lightTheme);
 
         //默认主题：idea亮色
         FlatLightLaf.setup();
         SwingUtilities.updateComponentTreeUI(frame);
-
-        //更深层次的主题更改
-        JComponent titleBar = (JComponent) frame.getRootPane().getComponent(1);
-        // 创建一个标签
-        JLabel label = new JLabel("Hello World");
-        label.setForeground(Color.WHITE);
-        // 在标题栏上添加标签
-        titleBar.add(label, BorderLayout.WEST);
 
         //设置窗口并显示
         frame.setSize(400, 400);
